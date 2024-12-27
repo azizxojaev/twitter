84280e283ddb8088cfa1afb8ef3221345e63757d
azizxojaev <azizxojaevbexruz@gmail.com>
2024-12-27 21:09:39 +0500

    create post template

--- twitter/urls.py
+++ twitter/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', home_page, name='home')
+    path('', home_page, name='home'),
+    path('create/', create_post_page, name='create'),
 ]
--- twitter/views.py
+++ twitter/views.py
@@ -7,6 +7,13 @@ def home_page(request):
     posts = Post.objects.all()
     context = {
         'posts': posts,
-        'url': 'home'
+        'page': 'home'
     }
     return render(request, 'home.html', context=context)
+
+def create_post_page(request):
+    print(request.user.is_authenticated)
+    context = {
+        'page': 'create'
+    }
+    return render(request, 'create.html', context=context)
