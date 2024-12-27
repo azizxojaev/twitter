d9d9a16336c38b9f6c2bfb2619aa3dbf3260d1a8
azizxojaev <azizxojaevbexruz@gmail.com>
2024-12-27 22:17:16 +0500

    login and register

--- twitter/urls.py
+++ twitter/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns = [
     path('', home_page, name='home'),
     path('create/', create_post_page, name='create'),
+    path('register/', register_page, name='register'),
+    path('login/', login_page, name='login'),
+    path('logout/', logout_page, name='logout'),
 ]
--- twitter/views.py
+++ twitter/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Post
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 
 def home_page(request):
@@ -12,8 +14,55 @@ def home_page(request):
     return render(request, 'home.html', context=context)
 
 def create_post_page(request):
-    print(request.user.is_authenticated)
-    context = {
-        'page': 'create'
-    }
-    return render(request, 'create.html', context=context)
+    if request.user.is_authenticated:
+        context = {
+            'page': 'create'
+        }
+
+        return render(request, 'create.html', context=context)
+    else:
+        return redirect('register')
+
+def register_page(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        context = {
+            'page': 'login'
+        }
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            if not User.objects.filter(username=username).exists():
+                user = User.objects.create_user(username=username, password=password)
+                login(request, user)
+                return redirect('home')
+            else:
+                context['error'] = 'Username already taken!'
+
+        return render(request, 'register.html', context=context)
+    
+
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        context = {
+            'page': 'login'
+        }
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+            else:
+                context['error'] = "Incorrect username or password"
+    
+    return render(request, 'login.html', context=context)
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('register')
